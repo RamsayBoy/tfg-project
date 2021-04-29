b12a479d808929124b33d85c0f88e9a6b0ef019c
Ramsay <38114695+RamsayBoy@users.noreply.github.com>
2021-04-29 21:01:01 +0200

    Some changes
    html en lang changed to es; save login page changes

--- front/src/app/auth/auth.module.ts
+++ front/src/app/auth/auth.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
 import { AuthRoutingModule } from './auth-routing.module';
 
-
 import { LoginComponent } from './pages/login/login.component';
+
+// Angular Material
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -16,6 +19,9 @@ import { LoginComponent } from './pages/login/login.component';
   imports: [
     CommonModule,
     AuthRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
   ]
 })
 export class AuthModule { }
