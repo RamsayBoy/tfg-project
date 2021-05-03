24b352da9dbb9279fd402f451fa03a3a4b078e84
Ramsay <38114695+RamsayBoy@users.noreply.github.com>
2021-05-04 00:17:42 +0200

    Update initializeScript.sql

--- database/initializeScript.sql
+++ database/initializeScript.sql
@@ -10,14 +10,35 @@ USE tfgdb;
 -- --------------------------------------------------------
 -- TABLES
 -- --------------------------------------------------------
-CREATE TABLE `user`(
+CREATE TABLE `role`(
 	id			INT UNSIGNED	AUTO_INCREMENT,
-    email		VARCHAR(256)	NOT NULL UNIQUE,
-    `password`  VARCHAR(64)     NOT NULL,        -- TODO: Check size (use 64 max. in the client due to limitations in certain hashing algorithms)
+    `name`      VARCHAR(16)     NOT NULL,
     
 	PRIMARY KEY(id)
 );
 
-INSERT INTO `user`(email, `password`)
+CREATE TABLE `user`(
+	id			INT UNSIGNED	AUTO_INCREMENT,
+    email		VARCHAR(256)	NOT NULL UNIQUE,
+    `password`  VARCHAR(64)     NOT NULL,        -- TODO: Check size (use 64 max. in the client due to limitations in certain hashing algorithms)
+    `role`      INT UNSIGNED,
+    
+	PRIMARY KEY(id),
+    
+    FOREIGN KEY(`role`)	REFERENCES `role`(id)
+        ON UPDATE CASCADE
+        ON DELETE SET NULL
+);
+
+-- --------------------------------------------------------
+-- DATA
+-- --------------------------------------------------------
+INSERT INTO `role`(`name`)
 VALUES
-    ('user1@tfg.es', 'user1pass');
+    ('admin');
+
+INSERT INTO `user`(email, `password`, `role`)
+VALUES
+    ('admin1@tfg.es', 'admin1pass', (SELECT id FROM `role` WHERE `name` = 'admin')),
+    ('user1@tfg.es', 'user1pass', NULL),
+    ('user2@tfg.es', 'user2pass', NULL);
