6d134d7cccfdb3a1944b8cf1c318e617b12c2a75
Ramsay <38114695+RamsayBoy@users.noreply.github.com>
2021-05-04 23:33:26 +0200

    Add some middlewares and dotenv

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 import authRoutes from './routes/auth.route';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // TODO: Add port, host, and api base url as environment variables
 const app = express();
 
 // Middleware
+app.use(express.json());
 app.use(helmet);
+app.use(cors());
 
 // Routes
 app.get('api/v0/auth', authRoutes);
