5bda862eb90aa79b7e0fdc872fadd1a0af83431d
Ramsay <38114695+RamsayBoy@users.noreply.github.com>
2021-05-22 20:52:55 +0200

    Add position fixed to toolbar

--- front/src/app/classes/class.module.ts
+++ front/src/app/classes/class.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ClassesComponent } from './pages/classes/classes.component';
 import { ClassRoutingModule } from './class-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { ClassComponent } from './components/class/class.component';
 
 
 
 @NgModule({
   declarations: [
     ClassesComponent,
+    ClassComponent,
   ],
   imports: [
     CommonModule,
