b0e9ed44f66b6007c5f295161aa19b5747b3b668
Ramsay <38114695+RamsayBoy@users.noreply.github.com>
2021-11-23 23:15:10 +0100

    Add form functionality to register new client

--- front/src/app/administration/administration.module.ts
+++ front/src/app/administration/administration.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { AdministrationComponent } from './pages/administration/administration.component';
 import { AdministrationRoutingModule } from './administration-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -13,7 +14,8 @@ import { AdministrationRoutingModule } from './administration-routing.module';
   imports: [
     CommonModule,
     SharedModule,
-    AdministrationRoutingModule
+    AdministrationRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class AdministrationModule { }
--- front/src/app/administration/pages/administration/administration.component.ts
+++ front/src/app/administration/pages/administration/administration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToolbarService } from 'src/app/shared/toolbar/services/toolbar.service';
 
 @Component({
@@ -8,12 +9,34 @@ import { ToolbarService } from 'src/app/shared/toolbar/services/toolbar.service'
 })
 export class AdministrationComponent implements OnInit {
 
+  public myClientsForm: FormGroup = new FormGroup({});
+  public newClientForm: FormGroup = new FormGroup({});
+  public configurationsForm: FormGroup = new FormGroup({});
+
   constructor(
     private toolbarService: ToolbarService,
+    private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit(): void {
     this.toolbarService.updateTitle("Administración");
+
+    this.newClientForm = this.formBuilder.group({
+      email: ['', [
+        // Validators.required,
+        Validators.email,
+      ]],
+    });
+  }
+
+  get email() {
+    return this.newClientForm.get('email');
+  }
+
+  onSubmitRegister(): void {
+    if (this.newClientForm.value.email) {
+      console.log(this.newClientForm.value)
+    }
   }
 
 }
