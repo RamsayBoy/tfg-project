1bf16b14e2f9d62798aa2165dda2435bdc456406
Ramsay <38114695+RamsayBoy@users.noreply.github.com>
2021-12-08 14:19:35 +0100

    Update some styles

--- front/src/app/clients/pages/clients/clients.component.ts
+++ front/src/app/clients/pages/clients/clients.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ToolbarService } from 'src/app/shared/toolbar/services/toolbar.service';
 
 @Component({
@@ -10,6 +11,7 @@ export class ClientsComponent implements OnInit, OnDestroy {
 
   constructor(
     private toolbarService: ToolbarService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
