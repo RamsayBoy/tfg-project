2606109f841cf47031c758091b084aa066d085ed
Ramsay <38114695+RamsayBoy@users.noreply.github.com>
2022-01-27 00:06:37 +0100

    Add new environment variables

--- back/src/config.ts
+++ back/src/config.ts
@@ -4,21 +4,27 @@ dotenv.config();
 
 const config = {
     api: {
-        BASE_URL : '/api/v0',
-        PORT     : Number(process.env.PORT),
-        HOST     : process.env.HOST || 'localhost',
+        BASE_URL                : '/api/v0',
+        PORT                    : Number(process.env.PORT),
+        HOST                    : process.env.HOST || 'localhost',
     },
     database: {
-        USER     : process.env.DB_USER,
-        PASSWORD : process.env.DB_PASS,
-        NAME     : process.env.DB_NAME,
+        USER                    : process.env.DB_USER,
+        PASSWORD                : process.env.DB_PASS,
+        NAME                    : process.env.DB_NAME,
     },
-    TOKEN_SECRET: process.env.TOKEN_SECRET || 'devToken',
-    DEFAULT_PROFILE_IMAGE_PATH: process.env.DEFAULT_PROFILE_IMAGE_PATH || '',
+    TOKEN_SECRET                : process.env.TOKEN_SECRET || 'devToken',
+    DEFAULT_PROFILE_IMAGE_PATH  : process.env.DEFAULT_PROFILE_IMAGE_PATH || '',
     mailer: {
-        user: process.env.NODEMAILER_USER,
-        pass: process.env.NODEMAILER_PASS,
-        devEmail: process.env.DEV_EMAIL
+        host                    : process.env.NODEMAILER_HOST,
+        port                    : Number(process.env.NODEMAILER_PORT),
+        secure                  : Boolean(process.env.NODEMAILER_SECURE),
+        user                    : process.env.NODEMAILER_USER,
+        pass                    : process.env.NODEMAILER_PASS,
+        devEmail                : process.env.DEV_EMAIL
+    },
+    clientApp: {
+        baseUrl                 : process.env.CLIENT_BASE_URL
     }
 }
 
--- back/src/services/mailer.service.ts
+++ back/src/services/mailer.service.ts
@@ -2,9 +2,9 @@ import nodemailer from 'nodemailer';
 import config from '../config';
 
 export const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    host: config.mailer.host,
+    port: config.mailer.port,
+    secure: config.mailer.secure,
     auth: {
         user: config.mailer.user,
         pass: config.mailer.pass
