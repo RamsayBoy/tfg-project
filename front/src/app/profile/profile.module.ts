e251c8bf3a0d757d73286db1354d5499d39586b1
Ramsay <38114695+RamsayBoy@users.noreply.github.com>
2022-05-09 20:18:09 +0200

    Update profile subfolder name to pages

--- front/src/app/profile/profile.module.ts
+++ front/src/app/profile/profile.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { ProfilePageComponent } from './pages/profile-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
