38bf746b34bd76c93c99f2ed75b9a6fb8f5f4296
Ramsay <38114695+RamsayBoy@users.noreply.github.com>
2022-07-15 16:45:03 +0200

    Remove update profile img hover effect

--- front/src/app/shared/dateAdapter/mondayAsFirstDay.ts
+++ front/src/app/shared/dateAdapter/mondayAsFirstDay.ts
@@ -1,7 +1,6 @@
 import { NativeDateAdapter } from '@angular/material/core';
 import {Injectable} from '@angular/core';
 
-// TODO: Make it works
 @Injectable()
 export class MondayAsFirstDay extends NativeDateAdapter {
   getFirstDayOfWeek(): number {
